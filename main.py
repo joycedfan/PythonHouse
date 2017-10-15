586c9e31b6624689de28b20c8ff12f995f6b78e4
joycedfan <joycedfan@gmail.com>
2017-10-15 00:23:28 -0700

    Added commands
    direction OR talk OR take OR fight OR hug

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from room import Room
+from item import Item
 from character import Enemy, Character, Friend
 
 kitchen = Room("Kitchen")
@@ -49,16 +50,80 @@ catrina = Friend("Catrina", "a tall friendly skeleton.")
 catrina.set_conversation("Why hello there, friend!")
 ballroom.set_character(catrina)
                          
+key = Item("key")
+key.set_description ("A small shiny key.")
+
+cheese = Item("cheese")
+cheese.set_description ("A cumbling block of smelly, rotting cheese.")
+
+swimming_pool.set_item(key)
+
+kitchen.set_item(cheese)
+                         
 current_room = kitchen
 
-while True:
+backpack = []
+
+dead = False
+
+while dead == False:
+
     print ("\n")
     current_room.get_details()
+    
+
+
     inhabitant = current_room.get_character()
     if inhabitant is not None:
       inhabitant.describe()
-
+      
+    inventory = current_room.get_item()
+    if inventory is not None:
+      inventory.describe()
 
     command = raw_input("> ")
-    current_room = current_room.move(command)
+    #check whether a direction was typed
+    if command in ["north", "south", "east", "west", "up", "down", "above", "below"]:
+      current_room = current_room.move(command)
+    elif command == "talk":
+      if inhabitant is not None:
+        inhabitant.talk()
+    elif command == "fight":
+      # You can check whether an object is an instance of a particular
+      # class with isinstance() - useful! This code means
+      # "If the character is a Friend"
+      if inhabitant == None or isinstance(inhabitant, Friend):
+        print("There is no one here to fight with.")
+      else:
+        print("What will you fight with?")
+        fight_with = input()
+        if fight_with in backpack:
+          
+          if inhabitant.fight(fight_with) == True:
+            print("Horray, you won the fight!")
+            print("Now to find a way out of here...")
+            current_room.character = None
+          else:
+            print("Ouch, you lost the fight.")
+            print("Your life flashes before your eyes. It slowly gets harder to see.")
+            print("Your vision blurs and the world around you fades away.")
+            print("You die.")
+            dead = True
+        else:
+            print("You realize you left your personal " + fight_with + " at home and turn to find one here.")
+    elif command == "hug":
+      if isinstance(inhabitant, Enemy):
+        print("I wouldn't do that if I were you...")
+      else:
+        inhabitant.hug()
+    elif command == "take":
+      if inventory is not None:
+        print ("You put the " + inventory.get_name() + " in your backpack.")
+        backpack.append(inventory.get_name())
+        current_room.set_item(None)
+      else:
+        print ("There is nothing here you can take.")
+        
+    else:
+      print("I don't know how to " + command + ".")
 
