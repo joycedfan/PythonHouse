8cfd51bb035f29e0c61921426385ad93d484a19a
joycedfan <joycedfan@gmail.com>
2017-10-15 00:17:40 -0700

    removed item location data from item.py

--- item.py
+++ item.py
@@ -3,7 +3,6 @@ class Item():
   def __init__(self, item_name):
     self.name = item_name
     self.description = None
-    self.item_location = {}
     
   def set_description(self, item_description):
     self.description = item_description
@@ -19,7 +18,3 @@ class Item():
   
   def describe(self):
     print (self.description)
-  
-  def print_items(self, current_room, all_items):
-    if current_room == self.item_location[self]:
-      print ("You see a " + self.name)
