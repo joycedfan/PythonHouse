b82cdd1279aef3dea27e6064aa3f9b6253ec5155
Joyce <joycedfan@gmail.com>
2021-10-17 11:58:13 -0700

    bugfix: modified formatting removed blank spaces, fixed missing bracket, made spacing consistent

--- room.py
+++ room.py
@@ -1,54 +1,54 @@
 class Room():
-  
+
   def __init__(self, room_name):
     self.name = room_name
     self.description = None
     self.linked_rooms = {}
     self.character = None
     self.item = None
-  
+
   def set_character (self, new_character):
     self.character = new_character
-    
+
   def set_item (self, new_item):
     self.item = new_item
 
   def get_item (self):
     return self.item
-     
+
   def get_character (self):
     return self.character 
-    
+
   def set_description(self, room_description):
     self.description = room_description
-      
+
   def get_description(self):
     return self.description
-        
+
   def describe(self):
     print(self.description)
-  
+
   def set_name(self, room_name):
     self.name = room_name
-    
+
   def get_name(self):
     return self.name
-    
+
   def link_room(self, room_to_link, direction):
     self.linked_rooms[direction] = room_to_link
     #print(self.name + " linked rooms :" + repr(self.linked_rooms))
-    
+
   def get_details(self):
-      print ( "The " + self.name)
-      print ("------------------")
-      self.describe()
-      for direction in self.linked_rooms:
-          room = self.linked_rooms[direction]
-          print( "The " + room.get_name() + " is " + direction + ".")
-          
+    print ( "The " + self.name)
+    print ("------------------")
+    self.describe()
+    for direction in self.linked_rooms:
+        room = self.linked_rooms[direction]
+        print( "The " + room.get_name() + " is " + direction + ".")
+
   def move(self, direction):
-      if direction in self.linked_rooms:
-          return self.linked_rooms[direction]
-      else:
-          print("You can't go that way.")
-          return self
+    if direction in self.linked_rooms:
+        return self.linked_rooms[direction]
+    else:
+        print("You can't go that way.")
+        return self
